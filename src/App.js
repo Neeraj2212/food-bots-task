48348a52634d93044cc5eae0a429c9eb4e5105e5
Neeraj Patil <patilneeraj2212@gmail.com>
2021-05-21 00:59:57 +0530

    Client side Routing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import {
+	BrowserRouter as Router,
+	Switch,
+	Route,
+	Redirect,
+} from "react-router-dom";
+import Profile from "./components/profile";
+import Summary from "./components/summary";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<Router>
+				<Switch>
+					<Route exact path={"/profile"} component={Profile} />
+					<Route exact path={"/summary"} component={Summary} />
+					<Redirect to="/profile" />
+				</Switch>
+			</Router>
+		</div>
+	);
 }
 
 export default App;
---
+++ src/components/profile.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ProfileComponent = () => {
+	return (
+		<div>
+			<h1>Hello Profile</h1>
+		</div>
+	);
+};
+
+export default ProfileComponent;
